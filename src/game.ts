e9013e39344e9a4daa439ed538ec763407e9cf46
Niko Heikkinen <niko.heikkinen3@gmail.com>
2021-08-19 21:23:54 +0300

    orangutan started

--- src/game.ts
+++ src/game.ts
@@ -1,20 +1,15 @@
-import { GameLoop, init } from 'kontra';
+import { GameLoop, init, initKeys } from 'kontra';
 import Orangutan from './orangutan';
 
 const { canvas } = init();
+initKeys();
 
-const orangutan = new Orangutan();
+const orangutan = new Orangutan(canvas);
 const loop = GameLoop({
   // create the main game loop
   update: () => {
     // update the game state
     orangutan.update();
-
-    // wrap the sprites position when it reaches
-    // the edge of the screen
-    if (orangutan.x > canvas.width) {
-      orangutan.x = -orangutan.width;
-    }
   },
   render: () => {
     // render the game state
--- src/orangutan.ts
+++ src/orangutan.ts
@@ -1,15 +1,51 @@
-import { Sprite } from 'kontra';
+import { keyPressed, Sprite } from 'kontra';
 
 class Orangutan extends Sprite.class {
-  constructor() {
+  constructor(private canvas: HTMLCanvasElement) {
     super({
-      x: 100, // starting x,y position of the sprite
-      y: 80,
+      x: 390, // starting x,y position of the sprite
+      y: 590,
       color: 'red', // fill color of the sprite rectangle
       width: 20, // width and height of the sprite rectangle
       height: 40,
-      dx: 2, // move the sprite 2px to the right every frame
     });
+  }
+
+  update(): void {
+    if (keyPressed('left')) {
+      this.dx = -2;
+      // left arrow pressed
+    } else if (keyPressed('right')) {
+      // right arrow pressed
+      this.dx = 2;
+    } else {
+      this.dx = 0;
+    }
+
+    console.log(this.dx);
+
+    if (keyPressed('up')) {
+      this.dy = -2;
+      // up arrow pressed
+    } else if (keyPressed('down')) {
+      this.dy = 2;
+      // down arrow pressed
+    } else {
+      this.dy = 0;
+    }
+    if (this.x > this.canvas.width - 20) {
+      this.dx = 0;
+      this.x = 780;
+    }
+    if (this.x < 0) {
+      this.dx = 0;
+      this.x = 0;
+    }
+    if (this.y > 600) {
+      this.dy = 0;
+      this.y = 600;
+    }
+    this.advance();
   }
 }
 
