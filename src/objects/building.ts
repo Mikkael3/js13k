266b55599df700c16e76d47d9af9b2815ced2e0b
Niko Heikkinen <niko.heikkinen3@gmail.com>
2021-08-22 14:52:50 +0300

    better building desctuction

--- src/objects/building.ts
+++ src/objects/building.ts
@@ -1,9 +1,26 @@
-import { getCanvas, Sprite } from 'kontra';
+import { GameObject, getCanvas, Sprite } from 'kontra';
 import collides from '../helpers/collides';
 import Play from '../scenes/play';
 import explodePool from './explode-pool';
 
-class Building extends Sprite.class {
+class BuildingPart extends Sprite.class {
+  constructor(x: number, y: number, public hp: number) {
+    super({
+      x: x,
+      y: y,
+      color: 'yellow',
+      width: 64,
+      height: 64,
+    });
+  }
+
+  handleHit = (): void => {
+    this.hp -= 1;
+    if (this.hp === 0) this.color = 'black';
+  };
+}
+
+class Building extends GameObject.class {
   constructor(
     x: number,
     y: number,
@@ -14,18 +31,35 @@ class Building extends Sprite.class {
     super({
       x: x,
       y: y,
-      color: 'cyan',
       width: 64 * width,
       height: 64 * height,
     });
+    this.createParts(width, height);
   }
+
+  createParts = (width: number, height: number): void => {
+    for (let i = 0; i < width; i++) {
+      for (let j = 0; j < height; j++) {
+        this.addChild(new BuildingPart(i * 64, j * 64, this.hp));
+      }
+    }
+  };
 
   update = (): void => {
     //void
     if (this.parent instanceof Play && this.parent.player) {
+      if (this.children.every((child) => child.hp <= 0)) {
+        this.parent.removeChild(this);
+        return;
+      }
       if (collides(this.parent.player, this)) {
-        this.hp -= 1;
-        if (this.hp >= 0) {
+        const parent = this.parent;
+        const part = this.children.find((child) =>
+          collides(child, parent.player)
+        );
+        //this.hp -= 1;
+        if (part && part.hp >= 0) {
+          part.handleHit();
           let i = 0;
           while (i < 50) {
             const y =
@@ -49,7 +83,6 @@ class Building extends Sprite.class {
             });
             i++;
           }
-          this.parent.removeChild(this);
         }
       }
     }
