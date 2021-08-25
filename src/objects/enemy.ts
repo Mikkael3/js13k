436f06a4458e1279b3547e4bd9a9f79ce856d2f5
Mikko Kemppainen <mikko.kemppainen@punamusta.com>
2021-08-26 01:22:35 +0300

    Add vision range to enemies

--- src/objects/enemy.ts
+++ src/objects/enemy.ts
@@ -8,7 +8,8 @@ class Enemy extends Sprite.class {
     y: number,
     width: number,
     height: number,
-    color: string
+    color: string,
+    public visionRange = 350
   ) {
     super({
       x: x,
@@ -22,18 +23,15 @@ class Enemy extends Sprite.class {
     const enemySpeed = 1;
     if (this.parent instanceof Play && this.parent.player) {
       const player = this.parent.player;
-      const direction = Vector(
-        player.x - this.x,
-        player.y - this.y
-      ).normalize();
-      this.dx = direction.x * enemySpeed;
-      this.dy = direction.y * enemySpeed;
-
+      const vectorToPlayer = Vector(player.x - this.x, player.y - this.y);
+      if (vectorToPlayer.length() < this.visionRange) {
+        this.dx = vectorToPlayer.normalize().x * enemySpeed;
+        this.dy = vectorToPlayer.normalize().y * enemySpeed;
+        this.advance();
+      }
       if (collides(this.parent.player, this)) {
         this.parent.removeChild(this);
       }
-
-      this.advance();
     }
   }
 }
