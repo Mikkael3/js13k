f2639bc9646ec0a6bdc6135c4f5da0694804f24b
Niko Heikkinen <niko.heikkinen3@gmail.com>
2021-08-26 23:30:05 +0300

    different tiles per zone

--- src/map.ts
+++ src/map.ts
@@ -1,11 +1,28 @@
 import { loadImage, TileEngine } from 'kontra';
 
 const getLayer = (): number[] => {
-  const data = [...Array(55 * 13).keys()].map((key: number) =>
+  const jungleTiles = [...Array(4 * 13).keys()].map((key: number) =>
+    key % 2 ? 19 : 20
+  );
+
+  const zone1Tiles = [...Array(14 * 13).keys()].map((key: number) =>
+    key % 4 ? 17 : 18
+  );
+
+  const zone2Tiles = [...Array(14 * 13).keys()].map(
+    (key: number) => 16 - (key % 4)
+  );
+
+  const zone3Tiles = [...Array(14 * 13).keys()].map(
+    (key: number) => 12 - (key % 2)
+  );
+
+  const data = [...Array(10 * 13).keys()].map((key: number) =>
     key % 5 ? 3 : 2
   );
-  return data;
+  return [...data, ...zone3Tiles, ...zone2Tiles, ...zone1Tiles, ...jungleTiles];
 };
+
 const getTileMap = async (): Promise<TileEngine> => {
   const asset = await loadImage('tileset.png');
   const tileEngineConfig = {
