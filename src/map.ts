df3039e0135323e2932e0aa4551d47a093ba37c7
Niko Heikkinen <niko.heikkinen3@gmail.com>
2021-09-04 21:23:04 +0300

    remove some tiles

--- src/map.ts
+++ src/map.ts
@@ -1,21 +1,17 @@
 import { TileEngine } from 'kontra';
 
 const getLayer = (): number[] => {
-  const jungleTiles = [...Array(4 * 13).keys()].map(() => 10);
+  const jungleTiles = [...Array(4 * 13).keys()].map(() => 9);
 
-  const zone1Tiles = [...Array(14 * 13).keys()].map((key: number) =>
-    key % 4 ? 7 : 14
-  );
+  const zone1Tiles = [...Array(14 * 13).keys()].map(() => 7);
 
   const zone2Tiles = [...Array(14 * 13).keys()].map(
-    (key: number) => 13 - (key % 2)
+    (key: number) => 12 - (key % 2)
   );
 
-  const zone3Tiles = [...Array(14 * 13).keys()].map(
-    (key: number) => 9 - (key % 2)
-  );
+  const zone3Tiles = [...Array(14 * 13).keys()].map(() => 8);
 
-  const data = [...Array(10 * 13).keys()].map(() => 11);
+  const data = [...Array(10 * 13).keys()].map(() => 10);
   return [...data, ...zone3Tiles, ...zone2Tiles, ...zone1Tiles, ...jungleTiles];
 };
 
